64429b408e36a3d895ce97478f8dcaf221969a7f
MelPC <melanyvp98@gmail.com>
2023-10-19 19:09:19 -0600

    actualizando botones signup

--- src/Componentes/SignUp/SignUp.js
+++ src/Componentes/SignUp/SignUp.js
@@ -1,6 +1,14 @@
 import { useState } from "react";
 import "./SingUp.css";
-import { Input, InputGroup, InputRightElement, Button } from "@chakra-ui/react";
+import {
+  Input,
+  InputGroup,
+  InputRightElement,
+  Button,
+  Stack,
+  ButtonGroup,
+} from "@chakra-ui/react";
+import { AiOutlineArrowRight } from 'react-icons/ai';
 
 export const SignUp = () => {
   const [show, setShow] = useState(false);
@@ -8,22 +16,37 @@ export const SignUp = () => {
 
   return (
     <div className="signUp-container">
-      <h1>Welcome to Food Tracker</h1>
+      <h1>Access Food Tracker</h1>
 
-      <Input variant="outline" placeholder="John Doe" aria-label="Name" />
-
-      <InputGroup size="md">
+      <Stack className="inputs-signUp">
+        <label for="input-name">Name</label>
         <Input
-          pr="4.5rem"
-          type={show ? "text" : "password"}
-          placeholder="Enter password"
+          variant="outline"
+          placeholder="John Doe"
+          className="name-signup"
         />
-        <InputRightElement width="4.5rem">
-          <Button h="1.75rem" size="sm" onClick={handleShow}>
-            {show ? "Hide" : "Show"}
-          </Button>
-        </InputRightElement>
-      </InputGroup>
+
+        <label for="input-name">Password</label>
+        <InputGroup className="password-signup">
+          <Input
+            pr="4.5rem"
+            type={show ? "text" : "password"}
+            placeholder="Enter password"
+          />
+          <InputRightElement width="4.5rem">
+            <Button h="1.75rem" size="sm" onClick={handleShow}>
+              {show ? "Hide" : "Show"}
+            </Button>
+          </InputRightElement>
+        </InputGroup>
+        <Button variant={"ghost"}>
+          <strong>Forgot my password</strong>
+        </Button>
+      </Stack>
+      <ButtonGroup>
+        <Button variant={"ghost"}>Create an account</Button>
+        <Button colorScheme='purple' rightIcon={<AiOutlineArrowRight/>} >Sign In</Button>
+      </ButtonGroup>
     </div>
   );
 };
