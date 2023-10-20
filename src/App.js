5abc6a6d62b0b795bdef4cc77a29625741ab1a82
MelPC <melanyvp98@gmail.com>
2023-10-19 18:19:52 -0600

    editando funcionalidad boton password

--- src/App.js
+++ src/App.js
@@ -1,29 +1,25 @@
-import { ChakraProvider } from '@chakra-ui/react'
-import { ReactDOM  } from 'react-dom/client';
+import { ChakraProvider } from "@chakra-ui/react";
+import { ReactDOM } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import './App.css';
-import { PreLoader } from './Componentes/PreLoader.js/PreLoader';
-import { PrincipalScreen } from './Componentes/PrincipalScreen/PrincipalScreen';
+import "./App.css";
+import { PreLoader } from "./Componentes/PreLoader.js/PreLoader";
+import { PrincipalScreen } from "./Componentes/PrincipalScreen/PrincipalScreen";
+import { SignUp } from "./Componentes/SignUp/SignUp";
 
 function App() {
   return (
     <>
       <ChakraProvider>
-      <PreLoader />
-      <PrincipalScreen />
+        <PreLoader />
+        {/* <PrincipalScreen /> */}
+        <SignUp />
+
         <BrowserRouter>
-          <Routes>
-            {/* <Route element={}/> */}
-
-          </Routes>
+          <Routes>{/* <Route element={}/> */}</Routes>
         </BrowserRouter>
-        
 
-        
       </ChakraProvider>
-      
     </>
-    
   );
 }
 
--- src/Componentes/SignUp/SignUp.js
+++ src/Componentes/SignUp/SignUp.js
@@ -1,9 +1,29 @@
-import "./SingUp.css"
+import { useState } from "react";
+import "./SingUp.css";
+import { Input, InputGroup, InputRightElement, Button } from "@chakra-ui/react";
 
 export const SignUp = () => {
-    return (
-        <>
-            
-        </>
-    )
-}
+  const [show, setShow] = useState(false);
+  const handleShow = () => setShow(!show);
+
+  return (
+    <div className="signUp-container">
+      <h1>Welcome to Food Tracker</h1>
+
+      <Input variant="outline" placeholder="John Doe" aria-label="Name" />
+
+      <InputGroup size="md">
+        <Input
+          pr="4.5rem"
+          type={show ? "text" : "password"}
+          placeholder="Enter password"
+        />
+        <InputRightElement width="4.5rem">
+          <Button h="1.75rem" size="sm" onClick={handleShow}>
+            {show ? "Hide" : "Show"}
+          </Button>
+        </InputRightElement>
+      </InputGroup>
+    </div>
+  );
+};
